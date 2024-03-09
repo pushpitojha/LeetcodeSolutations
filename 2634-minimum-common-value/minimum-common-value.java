c30b45ea8dce2f959954e5f9c230b3a74440f62d
Pushpit Ojha <72518995+pushpitojha@users.noreply.github.com>
2024-03-09 22:17:36 +0530

    Time: 2 ms (81.56%) | Memory: 62.4 MB (69.04%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.java
+++ 2634-minimum-common-value/minimum-common-value.java
@@ -3,24 +3,24 @@ class Solution {
         if(nums1.length==0 || nums2.length==0) {
             return -1;
         }
-        Set<Integer> set = new HashSet<>();
-        for(int i:nums1) {
-            set.add(i);
-        }
-        for(int i:nums2) {
-            if(set.contains(i)){
-                return i;
+        int x=0, y=0;
+        while(x<nums1.length && y<nums2.length) {
+            if(nums1[x]==nums2[y]) {
+                return nums1[x];
+            } else if(nums1[x]>nums2[y]) {
+                y++;
+            } else {
+                x++;
             }
         }
         return -1;
-        // int x=0, y=0;
-        // while(x<nums1.length && y<nums2.length) {
-        //     if(nums1[x]==nums2[y]) {
-        //         return nums1[x];
-        //     } else if(nums1[x]>nums2[y]) {
-        //         y++;
-        //     } else {
-        //         x++;
+        // Set<Integer> set = new HashSet<>();
+        // for(int i:nums1) {
+        //     set.add(i);
+        // }
+        // for(int i:nums2) {
+        //     if(set.contains(i)){
+        //         return i;
         //     }
         // }
         // return -1;
