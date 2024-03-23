a0fc6c1ef107143a3b3187ff217f05e407330adf
Pushpit Ojha <72518995+pushpitojha@users.noreply.github.com>
2024-03-23 16:28:45 +0530

    Time: 9 ms (57.96%) | Memory: 45.3 MB (10.34%) - LeetSync

---
+++ 383-ransom-note/ransom-note.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        if(ransomNote.length()>magazine.length()) {
+            return false;
+        }
+        Map<Character,Integer> map = new HashMap<>();
+        for(Character c:magazine.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0)+1);
+        }
+        for(Character c:ransomNote.toCharArray()){
+            if(map.containsKey(c) && map.get(c)>0) {
+                map.put(c, map.get(c)-1);
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
